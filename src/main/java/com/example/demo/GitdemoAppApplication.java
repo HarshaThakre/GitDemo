6778ef7ca23b20be4d04792ef9f4fa1d32e575cd
harsha.thakre <harsha@harshathakre24>
2022-06-14 15:44:41 +0530

    new message added

---
+++ src/main/java/com/example/demo/GitdemoAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		SpringApplication.run(GitdemoAppApplication.class, args);
+	}
+
+}
